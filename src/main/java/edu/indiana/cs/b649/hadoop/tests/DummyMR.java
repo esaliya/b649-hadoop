7ff75cc69374fa2f797b03f9dbe4272e379d9e6a
Saliya Ekanayake <esaliya@gmail.com>
2017-02-25 20:08:09 -0500

    All done

--- src/main/java/edu/indiana/cs/b649/hadoop/tests/DummyMR.java
+++ src/main/java/edu/indiana/cs/b649/hadoop/tests/DummyMR.java
@@ -1,11 +1,6 @@
-package edu.indiana.cs.b649.hadoop;
+package edu.indiana.cs.b649.hadoop.tests;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.StringTokenizer;
-
+import edu.indiana.cs.b649.hadoop.mr.WordCount;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -16,21 +11,26 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCount {
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+/**
+ * Saliya Ekanayake on 2/15/17.
+ */
+public class DummyMR {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+            extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
+            System.out.println(value.toString());
         }
     }
 
@@ -54,14 +54,14 @@ public class WordCount {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setJarByClass(DummyMR.class);
+        job.setMapperClass(DummyMR.TokenizerMapper.class);
+        job.setCombinerClass(DummyMR.IntSumReducer.class);
+        job.setReducerClass(DummyMR.IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path("src/main/resources/wc/words.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("src/main/resources/wc/output/" + dateFormat.format(new Date())));
+        FileInputFormat.addInputPath(job, new Path("src/main/resources/tests/maps.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("src/main/resources/tests/output/" + dateFormat.format(new Date())));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
